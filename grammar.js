a4e118f5f53e95fe28cf0f67b8daefea881d15bf
Daniel Fichtinger <daniel@ficd.ca>
2025-02-26 22:26:32 -0500

    feat: added support for global parameters
    chore: added annotation for LSP

--- grammar.js
+++ grammar.js
@@ -1,3 +1,6 @@
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
 module.exports = grammar({
   name: 'ini',
 
@@ -10,6 +13,7 @@ module.exports = grammar({
   rules: {
     document: $ => seq(
       repeat($._blank),  // Eat blank lines at top of file.
+      optional(repeat(seq($.setting))),
       repeat($.section),
     ),
 
